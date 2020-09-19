4960ac2aba6f4a95569566e9f31b133113b7b563
Andrii-Vekker <vekker304@gmail.com>
2020-09-19 22:33:49 +0300

    3-tasks

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// task-1
+// const name = '«Генератор защитного поля»';
+// let price = 1000;
+// const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
+// console.log(firstMessage);
+// price = 2000;
+// const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
+// console.log(secondMessage);
+
+// // task-2
+// const invoice = 1500;
+// const stock = 100;
+// Write code under this line
+// const message =
+//   invoice <= stock
+//     ? 'Заказ оформлен, с вами свяжется менеджер'
+//     : 'На складе недостаточно товаров!';
+// console.log(message);
+//console.log(message);
+
+//если invoice равен 100
+// то значение message будет равно
+// 'Заказ оформлен, с вами свяжется менеджер'
+
+//если invoice равен 50
+// то значение message будет равно
+// 'Заказ оформлен, с вами свяжется менеджер'
+
+// task-3
+
+// const userPassword = 'jqueryismyjam';
+
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+// const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+// let message;
+// if (userPassword === ADMIN_PASSWORD) {
+//   message = ACCESS_IS_ALLOWED;
+//   console.log(message);
+// } else if (userPassword === null) {
+//   message = CANCELED_BY_USER;
+//   console.log(message);
+// } else if (userPassword === 123) {
+//   message = ACCESS_DENIED;
+//   console.log(message);
+// } else {
+//   userPassword !== ADMIN_PASSWORD;
+//   message = ACCESS_DENIED;
+// }
+
+// task-3
