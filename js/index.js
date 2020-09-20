3f50ed67da4947118ebe6014ff92f0af8f5ef578
Andrii-Vekker <vekker304@gmail.com>
2020-09-20 12:55:45 +0300

    hw1-done

--- js/index.js
+++ js/index.js
@@ -51,3 +51,94 @@
 // }
 
 // task-3
+// const orderPieces = 10;
+
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+// let totalPrice = orderPieces * pricePerDroid;
+// let balanceCredit = credits - totalPrice;
+// let message;
+
+// if (orderPieces === null) {
+//   message = CANCELED_BY_USER;
+// } else if (credits >= totalPrice) {
+//   message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+// } else {
+//   message = ACCESS_DENIED;
+// }
+// console.log(message);
+// Write code under this line
+
+// console.log(message);
+
+//если orderPieces равно 4
+// то значение message будет равно
+// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+
+//если orderPieces равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если orderPieces равно 10
+// то значение message будет равно
+// 'Недостаточно средств на счету!'
+
+// task-4
+
+const countryName = 'КитаЙ';
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
+const CHINA = 'Китай';
+const AUSTRALIA = 'Австралия';
+const INDIA = 'Индия';
+const JAMAICA = 'Ямайка';
+let message;
+let price = 0;
+let country;
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
+  switch (country) {
+    case CHINA:
+      price = 100;
+      break;
+    case AUSTRALIA:
+      price = 170;
+      break;
+    case INDIA:
+      price = 80;
+      break;
+    case JAMAICA:
+      price = 120;
+      break;
+    default:
+      countryName !== country;
+      message = NO_DELIVERY;
+  }
+}
+if (price > 0) {
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+}
+console.log(message);
+// else if (countryName !== country) {
+//   message = NO_DELIVERY;
+// }
+
+// console.log(message);
+
+//если countryName равно "КитаЙ"
+// то значение message будет равно
+// 'Доставка в Китай будет стоить 100 кредитов'
+
+//если countryName равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если countryName равно "Чили"
+// то значение message будет равно
+// 'В выбранную страну доставка недоступна.'
