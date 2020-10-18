331d0270b1615ae5d1b4ffbe282c1fc2a5f1995e
Andrii-Vekker <vekker304@gmail.com>
2020-10-18 16:16:31 +0300

    dfsdgf

--- js/index.js
+++ js/index.js
@@ -735,35 +735,88 @@ mood : happy
 
 
 
-const account = {
-  owner: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['order-1', 'order-2', 'order-3'],
-  changeDiscount(value) {
-    discount = value; // Write code in this line
-  },
-  showOrders() {
-    return orders; // Write code in this line
-  },
-  addOrder(cost, order) {
-    balance -= cost; // Write code in this line
-    orders.push(order); // Write code in this line
-  },
-};
-const copyAccount =  Object.assign({},account);
-copyAccount.orders = [...account.orders];
+// const account = {
+//   owner: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['order-1', 'order-2', 'order-3'],
+//   changeDiscount(value) {
+//     this.discount = value; 
+//   },
+//   showOrders() {
+//     return this.orders; 
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost; 
+//     this.orders.push(order); 
+//   },
+// };
+// const copyAccount =  Object.assign({},account);
+// copyAccount.orders = [...account.orders];
 // копируем для автотестов ссылочные типы
 
 
-account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+//account.changeDiscount(0.15);
+//console.log(account.discount); // 0.15
 
-console.log(account.showOrders()); 
-// ['order-1', 'order-2', 'order-3']
+//console.log(account.showOrders()); 
+ //['order-1', 'order-2', 'order-3']
 
-account.addOrder(5000, 'order-4');
-console.log(account.balance); // 19000
+//account.addOrder(5000, 'order-4');
+//console.log(account.balance); // 19000
 
-console.log(account.showOrders());
+//console.log(account.showOrders());
 // ['order-1', 'order-2', 'order-3', 'order-4']
+
+//TASK-5
+
+// bind для замены this в методах объекта
+// Оформи вызов метода invokeInventoryAction таким образом, чтобы в качестве this для методов
+
+// inventory.add
+// inventory.remove выступал объект inventory
+
+
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     this.items.push(itemName);
+//     return `Adding ${itemName} to inventory`;
+//   },
+//   remove(itemName) {
+//     this.items = this.items.filter(item => item !== itemName);
+//     return `Removing ${itemName} from inventory`;
+//   },
+// };
+
+// const invokeInventoryAction = function(itemName, action) {
+//   const act = action(itemName);
+//   const msg =  `Invoking action on ${itemName}`;
+//   return {act, msg};
+// };
+
+// const invokeAdd = invokeInventoryAction(
+//   'Medkit',
+//   inventory.add.bind(inventory)  // Write code in this line
+// );
+// const arrayAdd = [...inventory.items];
+
+// console.log(invokeAdd);
+//{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
+
+// console.log(arrayAdd);
+// ['Knife', 'Gas mask', 'Medkit']
+
+// const invokeRemove = invokeInventoryAction(
+//   'Gas mask',
+//   inventory.remove.bind(inventory)  // Write code in this line
+// );
+
+// const arrayRemove = [...inventory.items];
+
+
+// console.log(invokeRemove);
+// //{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
+
+// console.log(arrayRemove);
+// // ['Knife', 'Medkit']
